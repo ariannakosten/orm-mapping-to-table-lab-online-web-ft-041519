be576047f6087a531702224d4e6de9d60fb44b8f
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-24 07:43:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,11 +32,16 @@ class Student
   
   def save
     sql = <<-SQL
-    
+    INSERT INTO students (name, grade)
+        VALUES (?, ?)
+    SQL
+
+     DB[:conn].execute(sql, self.name, self.grade)
   end
   
   def self.create(name:, grade:) #<--keyword arg
     student = Student.new(name, grade)
     student.save
     student
+  end
 end
